8185997e2adf23322dedc14808b707fa96f3be67
Raheem Kawojue <kawojue08@gmail.com>
2022-10-18 00:59:44 +0100

    done with new post page UI

---
+++ src/components/Post.jsx
@@ -0,0 +1,20 @@
+const Post = () => {
+    return (
+        <form className="new-post p-5">
+            <h1 className="text-3xl font-medium text-slate-600">New Post</h1>
+            <article className="flex flex-col gap-3 mt-5">
+                <div>
+                    <label htmlFor="title">Title: </label>
+                    <input type="text" id="title" />
+                </div>
+                <div>
+                    <label htmlFor="content">Post: </label>
+                    <textarea id="content"></textarea>
+                </div>
+                <button type="submit" className="py-1 bg-orange-500 rounded-md tracking-widest text-white text-xl hover:bg-orange-300 hover:text-slate-800 trans">Submit</button>
+            </article>
+        </form>
+    )
+}
+
+export default Post

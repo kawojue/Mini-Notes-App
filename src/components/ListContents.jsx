d7a9c908a4876f6dad785844f19a4a24ee8708ba
Raheem Kawojue <kawojue08@gmail.com>
2022-10-18 15:13:16 +0100

    setup a Link to single page

--- src/components/ListContents.jsx
+++ src/components/ListContents.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const ListContents = ({ posts }) => {
     return (
@@ -7,14 +7,14 @@ const ListContents = ({ posts }) => {
                 <article key={post.id} className="flex flex-col gap-5 w-full px-4 py-3 border-b-[1.5px] border-slate-400 text-slate-600">
                     <div>
                         <h3 className="capitalize w-fit tracking-wider text-2xl font-medium cursor-pointer">
-                            {post.title}
+                            <Link to={`/post/${post.id}`}>{post.title}</Link>
                         </h3>
                         <p className="mt-1 text-[0.75rem]">
                             {post.date}
                         </p>
                     </div>
                     <div>
-                        <h4>{post.content}</h4>
+                        <h4>{post.content.slice(0, 35) + "..."}</h4>
                     </div>
                 </article>
             ))}

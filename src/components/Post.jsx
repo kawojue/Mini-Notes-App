1e95acca163179abe671a92d48cd77d39a14cd44
Raheem Kawojue <kawojue08@gmail.com>
2022-10-18 15:13:52 +0100

    added a single page Preview

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+const Post = () => {
+    return (
+        <article className="p-5">
+            <h2>Header</h2>
+            <p className="mt-5 text-slate-600 text-lg">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, soluta iste aliquam, doloremque, voluptate est autem aliquid sed dolor corporis delectus amet voluptates voluptatem provident. Quas itaque sint a atque quasi, libero consequatur voluptatibus aliquid quia eius explicabo dignissimos, placeat doloremque corrupti. Accusamus ducimus porro itaque ea, eum facilis maxime aut cupiditate aperiam omnis! Deserunt.
+            </p>
+        </article>
+    )
+}
+
+export default Post

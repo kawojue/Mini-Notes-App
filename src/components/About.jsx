ab46c5a5ef5efc74088e323e2655cfb15c04a324
Raheem Kawojue <kawojue08@gmail.com>
2022-10-18 01:18:14 +0100

    created an about page

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <article className="p-5">
+            <h2>About</h2>
+            <p className="mt-5 text-slate-600 text-lg">
+                This is a practice project I created while I am Learning React.
+            </p>
+        </article>
+    )
+}
+
+export default About

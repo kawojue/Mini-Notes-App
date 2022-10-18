b5d3f86cc8e52ec30a29a6e980f175cc7a7713d9
Raheem Kawojue <kawojue08@gmail.com>
2022-10-18 16:54:51 +0100

    fixed the route to single page

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -7,6 +7,7 @@ const Post = ({ title, setTitle, content, setContent, onAddPost }) => {
         const splitted = content.split('')
         setCounter(splitted.length)
     }, [content])
+
     return (
         <form onSubmit={(e) => onAddPost(e)} className="new-post p-5">
             <h2>New Post</h2>
@@ -20,7 +21,7 @@ const Post = ({ title, setTitle, content, setContent, onAddPost }) => {
                     <p className="absolute top-0 right-0">{counter}</p>
                     <textarea id="content" required value={content} onChange={e => setContent(e.target.value)} ></textarea>
                 </div>
-                <button type="submit" className="py-1 bg-orange-500 rounded-md tracking-widest text-white text-xl hover:bg-orange-300 hover:text-slate-800 trans">Submit</button>
+                <button type="submit" className="btn trans">Submit</button>
             </article>
         </form>
     )

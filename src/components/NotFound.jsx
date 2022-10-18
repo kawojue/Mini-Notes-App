c0080ef0df8b77da5d8def5afaabb77a420c10ad
Raheem Kawojue <kawojue08@gmail.com>
2022-10-18 13:43:13 +0100

    added missing link page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <p className="mt-5 text-slate-600 text-lg">
+            Uh, oh! Page does not exist.
+        </p>
+    )
+}
+
+export default NotFound

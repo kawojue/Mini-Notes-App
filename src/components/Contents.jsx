78ea4b0f3ae12b296c1d2f93335c658291899046
Raheem Kawojue <kawojue08@gmail.com>
2022-10-18 13:44:23 +0100

    fixed post(s) error

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -3,7 +3,7 @@ import ListContents from './ListContents'
 const Contents = ({ posts }) => {
     return (
         <main className="bg-white overflow-y-auto">
-            <ListContents getFullDate={getFullDate} posts={posts} />
+            <ListContents posts={posts} />
         </main>
     )
 }

1db0bf06225f1bd64f77c3da1927449bf4199091
Raheem Kawojue <kawojue08@gmail.com>
2022-10-18 14:42:29 +0100

    used props drilling for addPost

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import NotFound from './components/NotFound'
 function App() {
   const getFullDate = format(new Date(), 'MMMM dd, yyyy pp')
   const [search, setSearch] = useState("")
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("")
+  const nav = useNavigate()
   const [posts, setPosts] = useState([
     {
       id: 1,
@@ -21,13 +24,50 @@ function App() {
 
 
 
+  const addPost = e => {
+    e.preventDefault()
+
+    let id = 0
+    const [IDList, newArr] = [[], []]
+    posts.forEach(post => {
+      IDList.push(post.id)
+    })
+
+    const getMax = Math.max(...IDList)
+
+    for (let i = 1; i <= getMax; i++) {
+      if (!IDList.includes(i)) {
+        newArr.push(i)
+      }
+    }
+
+    if (posts.length === getMax || posts.length === 0) {
+      id = posts.length + 1
+    } else {
+      id = newArr[0]
+    }
+
+    const newPost = { id, title: title.trim(), content: content.trim(), date: getFullDate }
+
+    setPosts([...posts, newPost])
+
+    nav('/')
+    window.location.reload()
+    setContent("")
+    setTitle("")
+  }
+
+  // const handlePost = (id) => {
+
+  // }
+
   return (
     <div className="container">
       <Routes>
         <Route path="/" element={<Structure search={search} onSetSearch={setSearch} />}>
           <Route path="/" element={<Contents posts={posts.filter(post => ((post.title).toLowerCase()).includes(search.toLowerCase()))} />} />
           <Route path="home" element={<Contents posts={posts} />} />
-          <Route path="post" element={<Posts posts={posts} onSetPosts={setPosts} getFullDate={getFullDate} />} />
+          <Route path="post" element={<Posts posts={posts} title={title} setTitle={setTitle} content={content} setContent={setContent} onAddPost={addPost} />} />
           <Route path="about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>

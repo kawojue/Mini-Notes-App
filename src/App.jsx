aa98a7f41a81a9fc95f71a61bd940f581bc798cc
Raheem Kawojue <kawojue08@gmail.com>
2022-10-20 11:06:59 +0100

    props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,45 @@
 import { useState, useEffect } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import { format } from 'date-fns'
+import { fillID } from './fillID'
 import Contents from "./components/Contents"
 import Post from './components/Post'
 import Posts from './components/Posts'
 import About from './components/About'
 import Structure from './components/Structure'
 import NotFound from './components/NotFound'
-import { fillID } from './fillID'
 
 function App() {
   const getFullDate = format(new Date(), 'MMMM dd, yyyy pp')
-  const API_URL = "http://localhost:5500/posts"
+  const API_URL = "http://localhost:5500/postss"
   const nav = useNavigate()
   const [search, setSearch] = useState("")
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
+  const [fetchErr, setFetchErr] = useState(false)
   const [posts, setPosts] = useState([])
-  const [fetchErr, setFetchErr] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const res = await fetch(API_URL)
-        return await res.json()
+        const data = await res.json()
+        if (!res.ok) throw new Error("Please, reload the page.")
+        return data
       } catch (err) {
-        setFetchErr(err.msg)
+        setFetchErr(true)
       }
     }
 
     const getPosts = async () => {
       const data = await fetchPosts()
       setPosts(data)
+      setIsLoading(false)
     }
 
     setTimeout(() => {
       getPosts()
-      setIsLoading(false)
     }, 1000)
   }, [])
 
@@ -58,9 +60,9 @@ function App() {
     <div className="container">
       <Routes>
         <Route path="/" element={<Structure search={search} onSetSearch={setSearch} />}>
-          <Route index element={<Contents posts={handleSearch} />} />
-          <Route path="home" element={<Contents posts={handleSearch} />} />
-          <Route path="post" element={<Posts posts={posts} title={title} setTitle={setTitle} content={content} setContent={setContent} onAddPost={addPost} />} />
+          <Route index element={<Contents posts={handleSearch} fetchErr={fetchErr} isLoading={isLoading} />} />
+          <Route path="home" element={<Contents posts={handleSearch} fetchErr={fetchErr} isLoading={isLoading} />} />
+          <Route path="post" element={<Posts title={title} setTitle={setTitle} content={content} setContent={setContent} onAddPost={addPost} />} />
           <Route path="about" element={<About />} />
           <Route path="/post/:id" element={<Post posts={posts} setPosts={setPosts} />} />
           <Route path="*" element={<NotFound />} />

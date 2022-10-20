9bc560fb1518cc9d4beefa96f7ff2cf320de999e
Raheem Kawojue <kawojue08@gmail.com>
2022-10-20 20:04:41 +0100

    mere change

--- src/App.jsx
+++ src/App.jsx
@@ -10,44 +10,56 @@ import Structure from './components/Structure'
 import NotFound from './components/NotFound'
 
 function App() {
+  const url = "http://localhost:5500/posts"
   const getFullDate = format(new Date(), 'MMMM dd, yyyy pp')
-  const API_URL = "http://localhost:5500/posts"
   const nav = useNavigate()
-  const [search, setSearch] = useState("")
+  const [posts, setPosts] = useState([])
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
+  const [search, setSearch] = useState("")
+  const [counter, setCounter] = useState(0)
   const [fetchErr, setFetchErr] = useState(null)
-  const [posts, setPosts] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const res = await fetch(API_URL)
-        const data = await res.json()
-        if (!res.ok) throw new Error("Please, reload the page.")
-        setPosts(data)
-      } catch (err) {
-        setFetchErr("Please, reload the page.")
-      }
+  const fetchPosts = async () => {
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      setPosts(data)
+    } catch (err) {
+      setFetchErr("Please, reload the page.")
     }
+  }
 
-
+  useEffect(() => {
     setTimeout(() => {
       (async () => await fetchPosts())()
       setIsLoading(false)
     }, 1000)
   }, [])
 
-  const addPost = e => {
+  useEffect(() => {
+    const splitted = content.split('')
+    setCounter(splitted.length)
+  }, [content])
+
+  const addPost = async e => {
     e.preventDefault()
-    const newPost = { id, title: title.trim(), content: content.trim(), date: getFullDate }
+
     const id = fillID(posts)
+    const newPost = { id, title: title.trim(), content: content.trim(), date: getFullDate }
     setPosts([...posts, newPost])
 
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newPost)
+    })
+
     nav('/')
-    setContent("")
-    setTitle("")
+
   }
 
   const handleSearch = posts.filter(post => ((post.title).toLowerCase()).includes(search.toLowerCase()))
@@ -58,9 +70,9 @@ function App() {
         <Route path="/" element={<Structure search={search} onSetSearch={setSearch} />}>
           <Route index element={<Contents posts={handleSearch} fetchErr={fetchErr} isLoading={isLoading} />} />
           <Route path="home" element={<Contents posts={handleSearch} fetchErr={fetchErr} isLoading={isLoading} />} />
-          <Route path="post" element={<Posts title={title} setTitle={setTitle} content={content} setContent={setContent} onAddPost={addPost} />} />
+          <Route path="post" element={<Posts title={title} setTitle={setTitle} content={content} setContent={setContent} counter={counter} onAddPost={addPost} />} />
           <Route path="about" element={<About />} />
-          <Route path="/post/:id" element={<Post posts={posts} setPosts={setPosts} />} />
+          <Route path="/post/:id" element={<Post posts={posts} setPosts={setPosts} url={url} />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>

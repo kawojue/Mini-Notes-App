df3421234ce62007c0acd3c7be88b898226b8dec
Raheem Kawojue <kawojue08@gmail.com>
2022-10-20 11:43:23 +0100

    Edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 const Footer = () => {
-    const getYear = new Date().getFullYear()
     return (
         <div className="px-2 py-1 bg-orange-400 text-center text-lg tracking-wider text-white mt-0.5">
-            <h5>Copyright &copy; {getYear}</h5>
+            <h5>Copyright &copy; {new Date().getFullYear()}</h5>
         </div>
     )
 }

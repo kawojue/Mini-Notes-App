7e495f1bf1086cfa8e45cce9e49fc5a207e3816b
Raheem Kawojue <kawojue08@gmail.com>
2022-10-20 20:06:00 +0100

    refractor

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,25 +1,16 @@
-import { useState, useEffect } from 'react'
-
-const Post = ({ title, setTitle, content, setContent, onAddPost }) => {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        const splitted = content.split('')
-        setCounter(splitted.length)
-    }, [content])
-
+const Posts = ({ title, content, setTitle, setContent, onAddPost, counter }) => {
     return (
         <form onSubmit={(e) => onAddPost(e)} className="new-post p-5">
             <h2>New Post</h2>
             <article className="flex flex-col gap-3 mt-5">
                 <div>
                     <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" required value={title} onChange={e => setTitle(e.target.value)} />
+                    <input type="text" id="title" required value={title} onChange={(e) => setTitle(e.target.value)} />
                 </div>
                 <div className='relative'>
                     <label htmlFor="content">Post:</label>
                     <p className="absolute top-0 right-0">{counter}</p>
-                    <textarea id="content" required value={content} onChange={e => setContent(e.target.value)} ></textarea>
+                    <textarea id="content" required value={content} onChange={(e) => setContent(e.target.value)} ></textarea>
                 </div>
                 <button type="submit" className="btn trans">Submit</button>
             </article>
@@ -27,4 +18,4 @@ const Post = ({ title, setTitle, content, setContent, onAddPost }) => {
     )
 }
 
-export default Post
+export default Posts

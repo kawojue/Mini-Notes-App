544221c3be9ed31ff186ea1d0090a0f0407d1c86
Raheem Kawojue <kawojue08@gmail.com>
2022-10-21 15:24:59 +0100

    Edit

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,4 @@
-const Posts = ({ title, content, setTitle, setContent, onAddPost, counter }) => {
+const Posts = ({ title, content, setTitle, setContent, onAddPost, countContent }) => {
     return (
         <form onSubmit={(e) => onAddPost(e)} className="new-post p-5">
             <h2>New Post</h2>
@@ -9,10 +9,10 @@ const Posts = ({ title, content, setTitle, setContent, onAddPost, counter }) => 
                 </div>
                 <div className='relative'>
                     <label htmlFor="content">Post:</label>
-                    <p className="absolute top-0 right-0">{counter}</p>
+                    <p className="absolute top-0 right-0">{countContent}</p>
                     <textarea id="content" required value={content} onChange={(e) => setContent(e.target.value)} ></textarea>
                 </div>
-                <button type="submit" className="btn trans">Submit</button>
+                <button type="submit" className="btn trans">Save</button>
             </article>
         </form>
     )

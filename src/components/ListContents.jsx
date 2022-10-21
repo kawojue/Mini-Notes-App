51472ae27306e80ab30594c5a5c4f902fe09d5cc
Raheem Kawojue <kawojue08@gmail.com>
2022-10-21 18:25:00 +0100

    Edit

--- src/components/ListContents.jsx
+++ src/components/ListContents.jsx
@@ -10,7 +10,7 @@ const ListContents = ({ posts, fetchErr, isLoading }) => {
                             <article key={post.id} className="flex flex-col gap-5 w-full px-4 py-3 border-b-[1.5px] border-slate-400 text-slate-600">
                                 <div>
                                     <h3 className="capitalize w-fit tracking-wider text-2xl font-medium cursor-pointer">
-                                        <Link to={`/post/${post.id}`}>{post.title}</Link>
+                                        <Link to={`/post/${post.id}`} className="hover:text-black">{post.title}</Link>
                                     </h3>
                                     <p className="mt-1 text-[0.75rem]">
                                         {post.datetime}

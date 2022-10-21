c3eb363f853e32ccfb5fcc1b7ca96767edc52ed3
Raheem Kawojue <kawojue08@gmail.com>
2022-10-21 21:57:56 +0100

    Edit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Navigation = ({ search, onSetSearch }) => {
     return (
         <div className="bg-black p-3 flex justify-between tracking-wide">
-            <input type="text" placeholder="Search Posts" className="focus:outline-none rounded-md pl-1 font-medium" value={search} onChange={e => onSetSearch(e.target.value)} />
+            <input type="text" placeholder="Search Notes" className="focus:outline-none rounded-md pl-1 font-medium" value={search} onChange={e => onSetSearch(e.target.value)} />
             <ul className="flex gap-3 text-white text-lg">
                 <li>
                     <Link to="home">Home</Link>

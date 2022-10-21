e0a2a172cd1dfd2acbea7994f52ec29ac41cc576
Raheem Kawojue <kawojue08@gmail.com>
2022-10-21 18:13:33 +0100

    fixed manual routing.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,21 +2,35 @@ import { useState, useEffect } from 'react'
 import { useParams, useNavigate, Link } from 'react-router-dom'
 import { formatFetch } from '../formatFetch'
 
-const Post = ({ posts, setPosts }) => {
-    const url = "http://localhost:5500/posts"
+const Post = ({ posts, setPosts, url }) => {
     const nav = useNavigate()
     const { id } = useParams()
     const [post, setPost] = useState({})
+    const [msg, setMsg] = useState("")
+    const { title, content, datetime } = post
+
+    const getIDS = []
+    posts.forEach(post => {
+        getIDS.push(post.id)
+    })
+
+    const manageID = getIDS.includes(parseInt(id))
 
     const getPostByID = async id => {
-        const res = await fetch(formatFetch(url, id))
-        const data = await res.json()
-        setPost(data)
-    }
+        try {
+            const res = await fetch(formatFetch(url, id))
+            if (!res.ok) throw new Error("Note not found!")
+            const data = await res.json()
+            setPost(data)
+        } catch (err) {
+            setMsg("Note not found!")
+        }
 
+    }
     useEffect(() => {
         (async () => await getPostByID(id))()
     }, [])
+
 
     const deletePost = async id => {
         const post = [...posts.filter(post => post.id != id)]
@@ -29,14 +43,17 @@ const Post = ({ posts, setPosts }) => {
     }
 
     return (
-        <article className="p-5">
-            <h2 className="capitalize mb-5">{post.title}</h2>
-            <p className="mt-5 text-slate-600 text-lg mb-5">{post.content}</p>
-            <div className="flex gap-5">
-                <button className="btn trans" onClick={() => deletePost(id)}>Delete</button>
-                <Link to={`/post/${id}/edit`} className="btn trans">Edit</Link>
-            </div>
-        </article>
+        <>
+            {manageID ? <article className="p-5">
+                <h2 className="capitalize mb-5">{title}</h2>
+                <p className='text-sm'>{datetime}</p>
+                <p className="mt-5 text-slate-600 text-lg mb-5">{content}</p>
+                <div className="flex gap-5">
+                    <button className="btn trans" onClick={() => deletePost(id)}>Delete</button>
+                    <Link to={`/post/${id}/edit`} className="btn trans">Edit</Link>
+                </div>
+            </article> : <p>{msg}</p>}
+        </>
     )
 }
 

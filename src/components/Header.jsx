608eed88b2eab61df4d0f1bba5d8e14a6b13cd7a
Raheem Kawojue <kawojue08@gmail.com>
2022-10-21 12:03:21 +0100

    changed title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ search, onSetSearch }) => {
         <header>
             <div className="px-2 py-4 bg-orange-400">
                 <h1 className="text-3xl text-black font-semibold tracking-wider">
-                    React JS Blog
+                    Notes App
                 </h1>
             </div>
             <Navigation search={search} onSetSearch={onSetSearch} />

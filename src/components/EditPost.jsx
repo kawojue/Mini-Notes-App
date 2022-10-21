c433de28cc42fce8c2015935bf7ed46a5febb695
Raheem Kawojue <kawojue08@gmail.com>
2022-10-21 12:23:54 +0100

    seting up Edit Post itself

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const EditPost = () => {
+    const { id } = useParams()
+    console.log(id)
     return (
-        <div>EditPost</div>
+        <form className="new-post p-5">
+            <h2>Edit Post</h2>
+            <article className="flex flex-col gap-3 mt-5">
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" id="title" required />
+                </div>
+                <div className='relative'>
+                    <label htmlFor="content">Post:</label>
+                    <p className="absolute top-0 right-0"></p>
+                    <textarea id="content" required  ></textarea>
+                </div>
+                <button type="submit" className="btn trans">Submit</button>
+            </article>
+        </form>
     )
 }
 

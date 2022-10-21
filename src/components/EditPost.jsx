f11864b8058695f68193f09799b87c8e81e0d979
Raheem Kawojue <kawojue08@gmail.com>
2022-10-21 18:23:52 +0100

    fixed manual routing.

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,16 +1,23 @@
 import { useNavigate, useParams } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { formatFetch } from '../formatFetch'
+import { manageID } from '../manageID'
 
 const EditPost = ({ url, posts, setPosts, editTitle, setEditTitle, editContent, setEditContent, getFullDateTime, countEditContent }) => {
     const { id } = useParams()
     const nav = useNavigate()
+    const [msg, setMsg] = useState("")
 
     const fetchPost = async id => {
-        const res = await fetch(formatFetch(url, id))
-        const data = await res.json()
-        setEditTitle(data.title)
-        setEditContent(data.content)
+        try {
+            const res = await fetch(formatFetch(url, id))
+            if (!res.ok) throw new Error("Note is not in the database.")
+            const data = await res.json()
+            setEditTitle(data.title)
+            setEditContent(data.content)
+        } catch (err) {
+            setMsg("Note is not in the database.")
+        }
     }
 
     useEffect(() => {
@@ -36,21 +43,25 @@ const EditPost = ({ url, posts, setPosts, editTitle, setEditTitle, editContent, 
     }
 
     return (
-        <form onSubmit={(e) => handleEdit(e)} className="new-post p-5">
-            <h2>Edit Post</h2>
-            <article className="flex flex-col gap-3 mt-5">
-                <div>
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" value={editTitle} onChange={(e) => setEditTitle(e.target.value)} required />
-                </div>
-                <div className='relative'>
-                    <label htmlFor="content">Post:</label>
-                    <p className="absolute top-0 right-0 text-sm">{countEditContent}</p>
-                    <textarea id="content" value={editContent} onChange={(e) => setEditContent(e.target.value)} required ></textarea>
-                </div>
-                <button type="submit" className="btn trans">Edit</button>
-            </article>
-        </form>
+        <>
+            {manageID(id, posts) ?
+                <form onSubmit={(e) => handleEdit(e)} className="new-post p-5">
+                    <h2>Edit Post</h2>
+                    <article className="flex flex-col gap-3 mt-5">
+                        <div>
+                            <label htmlFor="title">Title:</label>
+                            <input type="text" id="title" value={editTitle} onChange={(e) => setEditTitle(e.target.value)} required />
+                        </div>
+                        <div className='relative'>
+                            <label htmlFor="content">Post:</label>
+                            <p className="absolute top-0 right-0 text-sm">{countEditContent}</p>
+                            <textarea id="content" value={editContent} onChange={(e) => setEditContent(e.target.value)} required ></textarea>
+                        </div>
+                        <button type="submit" className="btn trans">Edit</button>
+                    </article>
+                </form> :
+                <p>{msg}</p>}
+        </>
     )
 }
 

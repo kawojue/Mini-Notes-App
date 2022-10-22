f2a27368ebab4f8828339dfe69f4eae3cb534c1f
Raheem Kawojue <kawojue08@gmail.com>
2022-10-22 17:31:07 +0100

    Edit

--- src/components/EditNote.jsx
+++ src/components/EditNote.jsx
@@ -38,6 +38,9 @@ const EditNote = ({ url, notes, setNotes, editTitle, setEditTitle, editContent, 
         })
 
         nav(`/note/${id}`)
+        setEditTitle("")
+        setEditContent("")
+
         window.location.reload()
     }
 

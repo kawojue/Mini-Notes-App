fc4af5db76afcd0fd5f955574176c0b55ff18664
Raheem Kawojue <kawojue08@gmail.com>
2022-10-23 09:49:08 +0100

    added overflow-auto, rem required

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,16 +1,16 @@
 const AddNote = ({ title, content, setTitle, setContent, onAddNote, countContent }) => {
     return (
-        <form onSubmit={(e) => onAddNote(e)} className="new-note p-5">
+        <form onSubmit={(e) => onAddNote(e)} className="new-note p-5 overflow-auto">
             <h2>Add New Note</h2>
             <article className="flex flex-col gap-3 mt-5">
                 <div>
                     <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" required value={title} onChange={(e) => setTitle(e.target.value)} />
+                    <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
                 </div>
                 <div className='relative'>
                     <label htmlFor="content">Note:</label>
                     <p className="absolute top-0 right-0">{countContent}</p>
-                    <textarea id="content" required value={content} onChange={(e) => setContent(e.target.value)} ></textarea>
+                    <textarea id="content" value={content} onChange={(e) => setContent(e.target.value)} ></textarea>
                 </div>
                 <button type="submit" className="btn trans">Save</button>
             </article>

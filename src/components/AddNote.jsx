2a322a1ea1de052462091a60a7eaa276d79b3c64
Raheem Kawojue <kawojue08@gmail.com>
2022-10-26 14:32:19 +0100

    setup useContext for data.

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,6 +1,10 @@
-const AddNote = ({ title, content, setTitle, setContent, onAddNote, countContent }) => {
+import DataContext from "../context/DataContext"
+import { useContext } from 'react'
+
+const AddNote = () => {
+    const { title, content, setTitle, setContent, addNote, countContent } = useContext(DataContext)
     return (
-        <form onSubmit={(e) => onAddNote(e)} className="new-note p-5 overflow-auto">
+        <form onSubmit={(e) => addNote(e)} className="new-note p-5 overflow-auto">
             <h2>Add New Note</h2>
             <article className="flex flex-col gap-3 mt-5">
                 <div>

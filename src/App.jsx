0d5c0423ac135068010dca978638b1a27d15dbc2
Raheem Kawojue <kawojue08@gmail.com>
2022-10-26 19:44:05 +0100

    Refractor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
 import Note from './components/Note'
 import About from './components/About'
+import Notes from './components/Notes'
 import AddNote from './components/AddNote'
-import Contents from './components/Contents'
-import EditNote from './components/EditNote'
 import NotFound from './components/NotFound'
+import EditNote from './components/EditNote'
 import Structure from './components/Structure'
+import { Routes, Route } from 'react-router-dom'
 import { DataProvider } from './context/DataContext'
 
 
@@ -15,9 +15,9 @@ function App() {
       <DataProvider>
         <Routes>
           <Route path="/" element={<Structure />}>
-            <Route index element={<Contents />} />
-            <Route path="home" element={<Contents />} />
-            <Route path="note" element={<AddNote />} />
+            <Route index element={<Notes />} />
+            <Route path="home" element={<Notes />} />
+            <Route path="note/add" element={<AddNote />} />
             <Route path="about" element={<About />} />
             <Route path="/note/:id" element={<Note />} />
             <Route path="/note/:id/edit" element={<EditNote />} />

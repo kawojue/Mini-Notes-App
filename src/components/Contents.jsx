94105f7abb671b9f5ad1da5ad4e8faaa3e5e3c63
Raheem Kawojue <kawojue08@gmail.com>
2022-10-26 14:32:53 +0100

    Edit

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -3,7 +3,7 @@ import Notes from './Notes'
 const Contents = ({ notes, fetchErr, isLoading }) => {
     return (
         <main className="bg-white overflow-y-auto">
-            <Notes notes={notes} fetchErr={fetchErr} isLoading={isLoading} />
+            <Notes />
         </main>
     )
 }

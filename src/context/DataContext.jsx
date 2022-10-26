44a9734c336107d6aaefe07daf6f280604d0805e
Raheem Kawojue <kawojue08@gmail.com>
2022-10-26 14:33:59 +0100

    More on useContext

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -7,9 +7,78 @@ import { useNavigate } from 'react-router-dom'
 const DataContext = createContext({})
 
 export const DataProvider = ({ children }) => {
+    const url = "http://localhost:5500/notes"
+    const getFullDateTime = format(new Date(), 'MMMM dd, yyyy pp')
+    const nav = useNavigate()
+    const [notes, setNotes] = useState([])
+    const [title, setTitle] = useState("")
+    const [search, setSearch] = useState("")
+    const [content, setContent] = useState("")
+    const [editTitle, setEditTitle] = useState("")
+    const [fetchErr, setFetchErr] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [editContent, setEditContent] = useState("")
+    const [countContent, setCountContent] = useState(0)
+    const [countEditContent, setCountEditContent] = useState(0)
+    const width = useWindowSize()
+
+    const fetchNotes = async () => {
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+            setNotes(data)
+        } catch (err) {
+            setFetchErr("Please, reload the page.")
+        }
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            (async () => await fetchNotes())()
+            setIsLoading(false)
+        }, 1000)
+    }, [])
+
+    const addNote = async e => {
+        e.preventDefault()
+
+        const id = fillID(notes)
+        const newNote = { id, title: title.trim(), content: content.trim(), datetime: getFullDateTime }
+        setNotes([...notes, newNote])
+
+        await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newNote)
+        })
+
+        setTitle("")
+        setContent("")
+
+        nav('/')
+    }
+
+    const handleSearch = notes.filter(note =>
+        ((note.title).toLowerCase()).includes(search.toLowerCase()) ||
+        ((note.content).toLowerCase()).includes(search.toLowerCase()) ||
+        ((note.datetime).toLowerCase()).includes(search.toLowerCase()))
+
+    useEffect(() => {
+        const splitContent = content.split('')
+        const splitEditContent = editContent.split('')
+        setCountContent(splitContent.length)
+        setCountEditContent(splitEditContent.length)
+    }, [content, editContent])
+
     return (
         <DataContext.Provider value={{
-
+            width, search, setSearch, handleSearch,
+            fetchErr, isLoading, notes, setNotes, url,
+            title, setTitle, content, setContent, addNote,
+            editTitle, setEditTitle, editContent, setEditContent,
+            countContent, countEditContent
         }}>
             {children}
         </DataContext.Provider>
